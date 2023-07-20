1fe17d9dbfd10503851e888fae01a48541ebc6c6
Investments-Dao <info.investments-dao@proton.me>
2023-07-20 08:03:13 +0200

    20/7

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,12 +10,58 @@ import Navbar from "@/components/navbar/Navbar";
 import { Nunito } from "next/font/google";
 import "../styles/globals.css";
 import getCurrentUser from "./actions/getCurrentUser";
+import type { Metadata } from 'next'
 
-export const metadata = {
-  title: "Inmo-Ocio",
-  description: "Inmuebles y Actividades",
-  icons: "/favicon.ico",
-};
+export const metadata: Metadata = {
+
+  title: "Inmo-Ocio | Investments-Dao",
+  description: "Anuncia tus inmuebles: compra, venta, alquiler de Inmuebles. Anuncia tus actividades de ocio: buceo, surf, pesca, Esqui, karting, despedidas de solteros, partyboad, etc",
+  category: 'inmobiliaria, turismo, ocio',
+  manifest: 'https://inmocio.investments-Dao.com/manifest.webmanifest.json',
+  icons: {
+    icon: {
+      url: "/icon.svg",
+      type: "image/svg",
+    },
+    shortcut: { url: "/icon-512.png", type: "image/png" },
+  },
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  verification: {
+    google: 'google',
+    yandex: 'yandex',
+    yahoo: 'yahoo',
+    other: {
+      me: ['my-email', 'my-link'],
+    },
+  },
+  alternates: {
+    canonical: 'https://inmocio.investments-dao.com',
+      
+    },
+    
+    }
+    
+    
+  
+  
+  
+  
+
+
+
+
 
 const font = Nunito({
   subsets: ["latin"],
@@ -31,7 +77,9 @@ export default async function RootLayout({
   return (
     <html lang="es">
       <body className={font.className}>
-        <ClientOnly>
+      <link rel="icon" href="/icon.ico" sizes="any" />
+      <link rel="apple-touch-icon" href="/assets/apple-touch-icon.png" sizes="140" />
+          <ClientOnly>
           <ToastContainerBar />
           <SearchModal />
           <RegisterModal />
